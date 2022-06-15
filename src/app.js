1f31ede7a2467f2961c90956af5e3b0a10dda5e4
MiguelUbeda <miguelubeda09@hotmail.com>
2022-06-15 19:36:38 +0000

    Clase

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  const heart = "fa-heart";
+  const spade = "fa-sun";
+  const club = "fa-moon";
+  const diamonds = "fa-bycicle";
+  let palosArray = [heart, spade, club, diamonds];
+  let randomPalos = Math.floor(Math.random() * 3);
+  let randomNumber = Math.floor(Math.random() * 12);
+  let palo = document.querySelectorAll("i");
+  let texto = document.querySelector("p");
+  let boton = document.querySelector("button");
+
+  boton.addEventListener("click", () => {
+    for (let i = 0; i < palo.length; i++) {
+      //HAY QUE REMOVER ANTES DE AÑADIR
+      palo[i].classList.add("fa-solid", palosArray[randomPalos]);
+    }
+    //HAY QUE REMOVER ANTES DE AÑADIR
+    texto.innerHTML = randomNumber;
+  });
 };
